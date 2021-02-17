f3fcde29c71ba0530f0fdb2943d2effb30ca00a7
yuanged <jianyuan@databus.cc>
2021-02-17 17:36:01 +0800

    problem 3: optimized sliding window

--- src/main/java/com/elemone/leetcode/longestsubstr3/Solution.java
+++ src/main/java/com/elemone/leetcode/longestsubstr3/Solution.java
@@ -5,7 +5,25 @@ import java.util.Map;
 
 public class Solution {
 
-    public int lengthOfLongestSubstring(String s) {
+    public int lengthOfLongestSubstring(String  s) {
+        if (s.length() == 0 || s.length() == 1) {
+            return s.length();
+        }
+        Map<Character, Integer> charIndex = new HashMap<>();
+        char curr;
+        int maxLen = 0;
+        for (int i = 0, j = 0 ; i < s.length(); i++) {
+            curr = s.charAt(i);
+            if (charIndex.containsKey(curr)) {
+                j = Math.max(charIndex.get(curr) + 1, j);
+            }
+            maxLen = Math.max(maxLen, i - j + 1);
+            charIndex.put(curr, i);
+        }
+        return maxLen;
+    }
+
+    public int lengthOfLongestSubstringOld(String s) {
         if (s.length() == 0 || s.length() == 1) {
             return s.length();
         }
